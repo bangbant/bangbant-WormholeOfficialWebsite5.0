69cbef2a069ca16d10da2829515d0cb3e232a266
Yin GuoDin <yin.guodin@wormholeslocal.com>
2022-11-21 17:58:28 +0800

    ygd

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import Header_ls from './Header.less';
+
+export default function Header() {
+  return (
+    <>
+      <div className={Header_ls.HeaderBox}></div>
+    </>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 import styles from './index.less';
-
+import Header from '../components/Header/Header';
 export default function IndexPage() {
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
