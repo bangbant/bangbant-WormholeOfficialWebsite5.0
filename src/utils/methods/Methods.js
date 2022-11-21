10c10fc29c32ca1c173b4433b517de195b8faba3
Yin GuoDin <yin.guodin@wormholeslocal.com>
2022-11-21 15:17:45 +0800

    ygd

---
+++ src/utils/methods/Methods.js
@@ -0,0 +1,28 @@
+// 数字缩减
+export function digitalreduction(data) {
+  console.log(data);
+  if (String(data).length < 4) {
+    return String(data);
+  } else if (String(data).length >= 4 && String(data).length < 7) {
+    return (
+      String(data).slice(0, String(data).length - 3) +
+      '.' +
+      String(data).slice(String(data).length - 3, String(data).length - 1) +
+      ' K'
+    );
+  } else if (String(data).length >= 7 && String(data).length < 10) {
+    return (
+      String(data).slice(0, String(data).length - 6) +
+      '.' +
+      String(data).slice(String(data).length - 6, String(data).length - 4) +
+      ' M'
+    );
+  } else if (String(data).length >= 10) {
+    return (
+      String(data).slice(0, String(data).length - 9) +
+      '.' +
+      String(data).slice(String(data).length - 9, String(data).length - 7) +
+      ' B'
+    );
+  }
+}
