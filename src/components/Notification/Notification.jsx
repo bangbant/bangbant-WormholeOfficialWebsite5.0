502b11625c8521ad3f18439a9054954409d04ee6
Yin GuoDin <yin.guodin@wormholeslocal.com>
2022-12-07 16:22:28 +0800

    ygd

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -5,12 +5,21 @@ import { history, Link } from 'umi';
 export default function Notification() {
   const [blocktop, setBlocktop] = useState('-320px');
   useEffect(() => {
-    setBlocktop('20px');
+    let timebs = String(new Date().getTime());
+    timebs = timebs.slice(0, 10);
+    if (Number(timebs) > Number(localStorage.getItem('storageTime'))) {
+      setBlocktop('20px');
+    }
   }, []);
   function close() {
     setBlocktop('-320px');
   }
   function PrivacyPolicy() {
+    let time = String(new Date().getTime());
+    time = time.slice(0, 10);
+    time = String(Number(time) + 259200);
+    localStorage.setItem('storageTime', time);
+
     setBlocktop('-320px');
     history.push('/PrivacyPolicy');
   }
--- src/pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -3,7 +3,11 @@ import PrivacyPolicy_ls from './PrivacyPolicy.less';
 export default function PrivacyPolicy() {
   return (
     <>
-      <div></div>
+      <div className={PrivacyPolicy_ls.PrivacyPolicyBox}>
+        <div className={PrivacyPolicy_ls.PrivacyPolicyBox_center}>
+          <p>Privacy Policy</p>
+        </div>
+      </div>
     </>
   );
 }
